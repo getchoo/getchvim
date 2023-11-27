f5395d89effc6c7b443923c09a7ce41f9d2a3399
seth <getchoo@tuta.io>
2023-11-26 19:58:26 -0500

    chore: don't use deprecated context_commentstring treesitter module

--- lua/getchoo/plugins/lsp.lua
+++ lua/getchoo/plugins/lsp.lua
@@ -19,11 +19,8 @@ require("nvim-treesitter.configs").setup({
 	auto_install = false,
 	highlight = { enable = true },
 	indent = { enable = true },
-	context_commentstring = {
-		enable = true,
-		enable_autocmd = false,
-	},
 })
 
----- trouble
+vim.g.skip_ts_context_commentstring_module = true
+
 require("trouble").setup()
