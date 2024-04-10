6616b478c0d3381ceba1adbe00d15a9f528d733b
seth <getchoo@tuta.io>
2024-04-09 20:25:11 -0400

    enable line numbers

--- config/lua/getchoo/init.lua
+++ config/lua/getchoo/init.lua
@@ -5,9 +5,11 @@ opt.tabstop = 2
 -- https://www.reddit.com/r/neovim/comments/14n6iiy/if_you_have_treesitter_make_sure_to_disable
 -- TLDR: this breaks things with treesitter indent
 opt.smartindent = false
+opt.number = true
 opt.wrap = true
 opt.syntax = "on"
 opt.termguicolors = true
+opt.mouse = "a"
 
 local backupDir = vim.fn.stdpath("state") .. "/backup"
 local b = io.open(backupDir, "r")
