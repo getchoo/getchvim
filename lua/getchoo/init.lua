b269a23c1ffb2ba2439aa003fb04039d68c818e7
Seth Flynn <getchoo@tuta.io>
2026-01-07 05:12:59 -0500

    plugins/treesitter: fix lazy loading

--- lua/getchoo/init.lua
+++ lua/getchoo/init.lua
@@ -23,3 +23,17 @@ g.loaded_node_provider = 0
 require("lz.n").load("getchoo/plugins")
 
 vim.cmd.colorscheme("catppuccin")
+
+vim.api.nvim_create_autocmd('FileType', {
+	pattern = { "*" },
+	callback = function(args)
+		local ft = args.match or vim.bo[args.buf].filetype
+		local language = vim.treesitter.language.get_lang(ft) or ft
+
+		if vim.treesitter.language.add(language) then
+			vim.treesitter.start(args.buf, language)
+			vim.bo[args.buf].syntax = "ON"
+			vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+		end
+	end,
+})
--- lua/getchoo/plugins/treesitter.lua
+++ lua/getchoo/plugins/treesitter.lua
@@ -1,12 +1,4 @@
 return {
 	"nvim-treesitter",
-	event = vim.tbl_extend("force", require("getchoo.utils").lazy_file, { "DeferredUIEnter" }),
-	after = function()
-		vim.api.nvim_create_autocmd('FileType', {
-			pattern = { '*' },
-			callback = function() vim.treesitter.start() end,
-		})
-
-		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-	end
+	event = require("getchoo.utils").lazy_file,
 }
